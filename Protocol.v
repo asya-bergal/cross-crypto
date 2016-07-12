9df15fed335ce3087d8e2aea3d9ec856b39d843a
Asya Bergal <asyabergal@gmail.com>
2016-07-12 12:44:23 -0400

    Changed guards representation to set, started working on valid transitions

--- Protocol.v
+++ Protocol.v
@@ -1,6 +1,7 @@
 Set Implicit Arguments.
 Unset Strict Implicit.
 Require Import Coq.Lists.List.
+Require Import Coq.Lists.ListSet.
 Import ListNotations.
 
 Load FirstOrder.
@@ -21,18 +22,28 @@ Section Protocol.
 
   Record sym_state := mkState {
       start: Q * list name;
-      agent_inputs: list handle;
-      attacker_outputs: list term;
-      guards: list closed_formula
+      attacker_inputs: list handle;
+      agent_outputs: list term;
+      guards: set closed_formula
   }.
 
-  Definition trans_seq := list sym_state.
+  Definition valid_transition (from to : sym_state) : Prop := True.
+  (* Definition valid_transition (from to : sym_state) : Prop := *)
+  (*   to.(guards) = set_add from.(guards) *)
+
+  Definition sym_trans_seq (pi : protocol) (l : list sym_state) : Prop :=
+    match l with
+      | to :: from :: xs => exists (t : transition), pi t /\ valid_transition from to
+      | _ => True
+    end.
  
   (* Why don't I need a formula as an argument here? *)
-  Definition satisfies_formulas (m : model) (l : list closed_formula) : Prop :=
+  Definition satisfies_formulas (m : model) (l : set closed_formula) : Prop :=
     fold_left (fun P => fun f => interp_closed_formula m (projT2 f) /\ P) l True. 
 
-  Definition valid (m : model) (tr : trans_seq) : Prop :=
+  (* Lists have last state at the head *)
+  (* Is it more reasonable to have this look like H -> content? *)
+  Definition valid (m : model) (pi : protocol) (tr : list sym_state) (H : sym_trans_seq pi tr) : Prop :=
     match tr with
       | [] => True
       | x :: _ => satisfies_formulas m x.(guards)
