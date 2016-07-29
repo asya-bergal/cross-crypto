78396cc22856a11fd6504ef8ab2885dc7ffe421e
Asya <abergal@mit.edu>
2016-07-29 00:25:24 -0400

    Computational interpretation with poly-proofs admitted and without names

--- src/HList.v
+++ src/HList.v
@@ -1,6 +1,9 @@
 Set Implicit Arguments.
 Unset Strict Implicit.
 
+Require Import Omega.
+Require Import CrossCrypto.FrapTactics.
+Require Import CrossCrypto.ListUtil.
 Require Import Coq.Lists.List.
 Import ListNotations.
 
@@ -11,13 +14,28 @@ Inductive hlist A (f : A -> Type) : list A -> Type :=
 Notation " h[] " := (hnil _).
 Infix "h::" := hcons (at level 60, right associativity).
 
+Definition hhead A f (l : list A) (hl : hlist f l) (H : l <> []) : f (head_with_proof H).
+  inversion hl.
+  congruence.
+  subst l.
+  simpl.
+  exact X.
+Defined.
+
+Fixpoint hnth (n : nat) A f (l : list A) (hl : hlist f l) (H : length(l) > n) {struct l}: f (nth_with_proof l H).
+  cases n; cases l; simpl in H; try omega.
+  inversion hl.
+  exact X.
+  inversion hl.
+  refine (hnth n A f l X0 _).
+Defined.
 
 Definition hmap A (f : A -> Type) B (g : B -> Type)
            (F : A -> B) (F' : forall a, f a -> g (F a))
            (l : list A) (h : hlist f l) : hlist g (map F l).
   induction h; constructor; auto.
 Defined.
-
+ 
 Definition hmap' A (f : A -> Type) (g : A -> Type)
            (F' : forall a, f a -> g a)
            (l : list A) (h : hlist f l) : hlist g l.
