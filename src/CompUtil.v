8a53a2688407cf58063b93aa33f04f546aacb382
Asya <abergal@mit.edu>
2016-08-04 16:11:17 -0400

    More work on computational interpretations

---
+++ src/CompUtil.v
@@ -0,0 +1,31 @@
+Set Implicit Arguments.
+
+Require Import Coq.Lists.List.
+Import ListNotations.
+
+Require Import FCF.
+
+Require Import CrossCrypto.FrapTactics.
+Require Import CrossCrypto.HList.
+Require Import CrossCrypto.Tuple.
+
+(* This is a weird way of doing things but I couldn't figure out a good workaround *)
+Definition set2type (A : Set) : Type := A.
+
+Fixpoint bind_comps (comp_types: list Set) (h : hlist Comp comp_types) T (f : hlist set2type comp_types -> Comp T) {struct comp_types} : Comp T.
+  cases comp_types.
+  exact (f h[]).
+  refine (bind_comps comp_types (htail h) T (fun (f' : hlist set2type comp_types) => Bind _ (fun x => f (_ h:: f')))).
+  refine (hhead h _).
+  equality.
+  exact x.
+Defined.
+
+Definition bind_tuple T {eta : nat} n (f : tuple (Bvector eta) n -> Comp T) : Comp T.
+  refine (bind_comps (list2hlist (repeat (Rnd eta) n)) _).
+  refine (fun h => f (hlist2tuple h _ )).
+  assert (length (repeat (Rnd eta) n) = n).
+  apply repeat_length.
+  rewrite H.
+  equality.
+Defined.
--- src/HList.v
+++ src/HList.v
@@ -2,8 +2,9 @@ Set Implicit Arguments.
 Unset Strict Implicit.
 
 Require Import Omega.
-Require CrossCrypto.FrapTactics.
+Require Import CrossCrypto.FrapTactics.
 Require Import CrossCrypto.ListUtil.
+Require Import CrossCrypto.Tuple.
 Require Import Coq.Lists.List.
 Import ListNotations.
 
@@ -22,6 +23,71 @@ Definition hhead A f (l : list A) (hl : hlist f l) (H : l <> []) : f (head_with_
   exact X.
 Defined.
 
+Definition htail A f (l : list A) (hl : hlist f l) : hlist f (tl l).
+  cases l.
+  exact h[].
+  inversion hl.
+  exact X0.
+Defined.
+
+Fixpoint list2hlist T (A : T) (f : T -> Type) (fl : list (f A)) : hlist f (repeat A (length fl)).
+  cases fl.
+  exact (h[]).
+  exact (f0 h:: list2hlist T A f fl).
+Defined.
+
+Fixpoint hlist2list A (f : A -> Type) l (hl : hlist f l) (x : A) (H : exists n , l = repeat x n) : list (f x).
+  cases hl.
+  exact [].
+  refine (_ :: hlist2list A f l hl x _).
+  assert (x = x0).
+  destruct H.
+  cases x1.
+  simplify.
+  equality.
+  unfold repeat in H.
+  equality.
+  subst x.
+  exact f0.
+  destruct H.
+  cases x1.
+  simplify.
+  equality.
+  exists x1.
+  unfold repeat in H.
+  assert (x = x0).
+  equality.
+  subst x.
+  unfold repeat.
+  equality.
+Defined.
+
+Fixpoint tuple2hlist A (T : A) P n (t : tuple (P T) n)
+: hlist P (repeat T n).
+  induction t; constructor; assumption.
+Defined.
+
+Lemma hlist2list_len : forall A (f : A -> Type) (l : list A) (hl : hlist f l) (x : A) (P : exists n, l = repeat x n), length (hlist2list hl P) = length l.
+Proof.
+  induct hl; simplify; equality.
+Qed.
+
+Definition hlist2tuple (A : Type) (f : A -> Type) (l : list A) (hl : hlist f l) (x : A) n (P : l = repeat x n) : tuple (f x) n.
+  assert (exists n0 : nat, l = repeat x n0).
+  exists n.
+  assumption.
+  assert (n = length (hlist2list hl (x:=x) H)).
+  assert (length (hlist2list hl (x:=x) H) = length l).
+  apply hlist2list_len.
+  rewrite H0.
+  rewrite P.
+  Locate repeat.
+  symmetry.
+  apply repeat_length.
+  rewrite H0.
+  exact (list2tuple (hlist2list hl H)).
+Defined.
+        
 Definition hmap A (f : A -> Type) B (g : B -> Type)
            (F : A -> B) (F' : forall a, f a -> g (F a))
            (l : list A) (h : hlist f l) : hlist g (map F l).
@@ -36,3 +102,4 @@ Definition hmap' A (f : A -> Type) (g : A -> Type)
   clear f g F' h.
   induction l as [| x xs IHl]; [| simpl; rewrite IHl]; reflexivity.
 Defined.
+
